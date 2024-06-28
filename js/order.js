a6747dfc4041ec7652bd3cbffe7a464c0440d4a8
AFLOAT <120665000+afloat01@users.noreply.github.com>
2024-06-28 12:02:22 +0200

    Add files via upload

---
+++ js/order.js
@@ -0,0 +1,52 @@
+let l = document.getElementById("l")
+   let m = document.getElementById("m")
+   let s = document.getElementById("s")
+   let xl = document.getElementById("xl")
+   let xxl = document.getElementById("xxl")
+   let xxxl = document.getElementById("xxxl")
+   let p_size = document.getElementById("p_size")
+   
+   const stt = "0px solid black"
+   
+   l.onclick = function(){
+    p_size.value = l.innerHTML
+    l.style.borderBottom=stt
+    
+   }
+   
+   m.onclick = function(){
+   p_size.value = m.innerHTML
+   m.style.borderBottom=stt
+   }
+   
+   s.onclick = function(){
+   p_size.value = s.innerHTML
+   s.style.borderBottom=stt
+   }
+   
+   xl.onclick = function(){
+   p_size.value = xl.innerHTML
+   xl.style.borderBottom=stt
+   }
+   
+   xxl.onclick = function(){
+   p_size.value = xxl.innerHTML
+   xxl.style.borderBottom=stt
+   }
+   
+   xxxl.onclick = function(){
+   p_size.value = xxxl.innerHTML
+   xxxl.style.borderBottom=stt
+   }
+   
+   
+    const sub = document.getElementById("sub")
+    const add = document.getElementById("add")
+    let num = document.getElementById("num")
+    
+    sub.onclick = function(){
+     num.value--
+    }
+    add.onclick = function(){
+     num.value++
+    }
